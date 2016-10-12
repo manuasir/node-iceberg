65edf270773d62cff810614b360a26cfdbcb7bf8
Manuel J. B <manu.asi2@gmail.com>
2016-10-13 00:40:14 +0200

    modificando tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,53 @@
-var Crawler = require('../app/crawler');
+var assert = require("assert"),
+    arbol = require(__dirname+"/../app/tree.js"),
+    nodo = require(__dirname+"/../app/node.js"),
+    crawler = require(__dirname+"/../app/crawler.js");
 
-var arania = new Crawler("http://www.w3schools.com/");
+describe('Nodo', function(){
+    // Testea que se haya cargado bien la librera
+    describe('Carga', function(){
+    it('should be loaded', function(){
+        assert(nodo, "Cargado");
+    });
 
-function iniciar(callback){
-	arania.arrancar(arania.getPrimeraUrl(),arania.getArbol(),-1,1)
-	.then(function(){
-		console.log("Saliendo del proceso");
-		arania.recorrerArbol(function(){
-			console.log("Terminado de guardar MongoDB");
-			return callback();
-		});
-		
-	})
-}
+    });
+    describe('Crea', function(){
+    it('should create apuestas correctly', function(){
+        var node = new nodo('Polopos','Alhama','2-3');
+        assert.equal(node.as_string(), "Polopos: Alhama - 2-3","Creado");
+    });
+    });
+});
 
-iniciar(function(){
-	process.exit();
+describe('Arbol', function(){
+    // Testea que se haya cargado bien la librera
+    describe('Carga', function(){
+    it('should be loaded', function(){
+        assert(arbol, "Cargado");
+    });
+
+    });
+    describe('Crea', function(){
+    it('should create apuestas correctly', function(){
+        var tree = new arbol('Polopos');
+        assert.equal(nuevo_usuario.as_string(), "Polopos");
+    });
+    });
+});
+
+
+describe('Crawler', function(){
+    // Testea que se haya cargado bien la librera
+    describe('Carga', function(){
+    it('should be loaded', function(){
+        assert(crawler, "Cargado");
+    });
+
+    });
+    describe('Crea', function(){
+    it('should create apuestas correctly', function(){
+        var crawl = new crawler('http://prueba.com');
+        assert.equal(crawl.getUrlRaiz(), "http://prueba.com");
+    });
+    });
 });
