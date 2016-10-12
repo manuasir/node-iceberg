cde6a57f54746ab516a0c6026ddc6b2d0a3d68c5
Manuel J. B <manu.asi2@gmail.com>
2016-10-13 00:53:20 +0200

    probando travis

--- test/test.js
+++ test/test.js
@@ -3,17 +3,6 @@ var assert = require("assert"),
     nodo = require(__dirname+"/../app/node.js"),
     crawler = require(__dirname+"/../app/crawler.js");
 
-describe('Arbol', function(){
-    // Testea que se haya cargado bien la librera
-    describe('Carga', function(){
-    it('should be loaded', function(){
-        assert(arbol, "Cargado");
-    });
-
-    });
- 
-});
-
 
 describe('Crawler', function(){
     // Testea que se haya cargado bien la librera
