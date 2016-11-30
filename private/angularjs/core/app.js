45a336146ffb893f90d1abf6585dede1c46c7bc4
manuasir <manu.asi2@gmail.com>
2016-11-30 23:41:17 +0100

    Refracting codigo :+1: closes #12

---
+++ private/angularjs/core/app.js
@@ -0,0 +1,14 @@
+
+/**
+ * Created by manu on 20/10/16.
+ * Core library
+ */
+
+var miApp = angular.module('miApp', [
+    "jsonFormatter",
+    "ngRoute",
+    'ui.router',
+    "satellizer",
+    "ngMaterial",
+    "ngCookies"
+]);
