d8d172dfd72464dd89fd0ea6fcefdfe10ef69c1e
manuasir <manu.asi2@gmail.com>
2016-12-02 00:59:40 +0100

    Arreglando errores de produccion :+1:

--- routes/routes.js
+++ routes/routes.js
@@ -33,53 +33,53 @@ router.get('/', function(req, res, next) {
 /**
  * Ruta que comienza a procesar una cadena
  */
-// router.get('/model/:cadena', function(req, res){
-//     var cadena = req.body.cadena;
-//     var string = req.params.cadena;
-//     if(string.indexOf("/") > -1) {
-//         var url = 'http://'+string;
-//     }
-//     else
-//         var url = 'http://'+string+"/";
-//
-//     //console.log("buscando: "+url);
-//     // console.log(url);
-//     var query = Nodo.find({ datos: url});
-//     query.exec(function(err, model){
-//         if(err){
-//             req.json(err);
-//         }
-//         else{
-//             if(!isEmptyObject(model)){
-//                 // console.log("NO VACIO");
-//                 res.json(model);
-//             }
-//             else{
-//                 // console.log("NO HAY DATOS, BUSCANDO...");
-//
-//                 var arania = new Crawler(url);
-//
-//                 function iniciar(callback){
-//                     arania.arrancar(arania.getPrimeraUrl(),arania.getArbol(),-1,2)
-//                         .then(function(){
-//                             //  console.log("Saliendo del proceso");
-//                             arania.recorrerArbol(function(){
-//                                 // console.log("Terminado de guardar MongoDB");
-//                                 return callback();
-//                             });
-//
-//                         })
-//                 }
-//                 iniciar(function(){
-//                     query.exec(function(err, model){
-//                         if(!err)
-//                             res.json(model);
-//                     });
-//                 });
-//             }
-//         }
-//     });
-// });
+router.get('/model/:cadena', function(req, res){
+    var cadena = req.body.cadena;
+    var string = req.params.cadena;
+    if(string.indexOf("/") > -1) {
+        var url = 'http://'+string;
+    }
+    else
+        var url = 'http://'+string+"/";
+
+    //console.log("buscando: "+url);
+    // console.log(url);
+    var query = Nodo.find({ datos: url});
+    query.exec(function(err, model){
+        if(err){
+            req.json(err);
+        }
+        else{
+            if(!isEmptyObject(model)){
+                // console.log("NO VACIO");
+                res.json(model);
+            }
+            else{
+                // console.log("NO HAY DATOS, BUSCANDO...");
+
+                var arania = new Crawler(url);
+
+                function iniciar(callback){
+                    arania.arrancar(arania.getPrimeraUrl(),arania.getArbol(),-1,2)
+                        .then(function(){
+                            //  console.log("Saliendo del proceso");
+                            arania.recorrerArbol(function(){
+                                // console.log("Terminado de guardar MongoDB");
+                                return callback();
+                            });
+
+                        })
+                }
+                iniciar(function(){
+                    query.exec(function(err, model){
+                        if(!err)
+                            res.json(model);
+                    });
+                });
+            }
+        }
+    });
+});
 
 
 module.exports = router;
