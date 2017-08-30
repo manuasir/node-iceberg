05b35add7dacf9f7e7a813a6777aa94446dd5566
manuasir <manu.asi2@gmail.com>
2017-08-30 18:22:27 +0200

    mergeando en develop

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,12 @@
 
+var Nodo            = require('../private/models/model.js');
 var path = require('path');
 var express = require('express');
 var router = express.Router();
 var Crawler = require('../private/clases/crawler');
 var mongoose = require('mongoose');
-var util = require('./../private/clases/utils')
-var confs = require('../private/clases/configurations')
-var _ = require('lodash')
-var functions = require('./functions')
-// CONECTAR CON MONGODB //
-// mongoose.connect('mongodb://manuasir:mongodb@ds147072.mlab.com:47072/heroku_mctx4f0c');
-mongoose.connect('mongodb://localhost:27017/crawler');
+mongoose.connect('mongodb://manuasir:mongodb@ds147072.mlab.com:47072/heroku_mctx4f0c');
+// mongoose.connect('mongodb://localhost:27017/crawler');
 
 /**
  * Renderiza la vista principal con cabeceras,footer,etc
@@ -23,27 +19,36 @@ router.get('/', function(req, res, next) {
  * Ruta que comienza a procesar una cadena
  */
 router.get('/crawl/', function(req, res){
-  if(!req.query.q || !req.query.level || !req.query.srv)
+  var url=""
+  if(!req.query.q || !req.query.level)
     return res.status(400).json({code:"faltan parametros de busqueda"});
+  var string = req.query.q;
+  var level = (req.query.level < 5) ? req.query.level : 5;
+  console.log("nivel...",level)
+  if(string.indexOf("/") > -1) {
+    url = 'http://'+string;
+  }
+  else
+    url = 'http://'+string+"/";
 
-  var url = util.urlValid(req.query.q);
-  var level = (req.query.level < 300) ? req.query.level : 300;
-  var srv = req.query.srv;
-  var crawl = new Crawler(url,level);
-
-  // cargar configuración predeterminada (ej. blogspot)
-  var conf = confs.services(srv)
-
-  crawl.arrancar(crawl.getPrimeraUrl(),crawl.getArbol(),-1,level,conf,function(err,data){
+  var arania = new Crawler(url,level);
+  console.log('arrancando')
+  arania.arrancar(arania.getPrimeraUrl(),arania.getArbol(),-1,level,true,function(err,data){
     if(err){
       console.error("terminado con error ",err)
       return res.status(401).json(err);
     }
-    crawl.insertTreeIntoDb(function(err,data){
+    console.log("terminado sin error, guardando en mongodb ")
+    arania.insertTreeIntoDb(function(err,data){
       if(err)
         return res.status(402).json(err);
-      var aux = _.join(_.map(functions.getPayloads(data),'payload'))
-      res.status(200).json(aux);
+      var query = Nodo.find({ url: url});
+      query.exec(function(err, model){
+        if(err)
+          return res.status(402).json(err);
+        console.log("devuelvo el modelo")
+        res.status(200).json(model);
+      });
     });
   })
 });
