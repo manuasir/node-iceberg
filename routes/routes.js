9ccf54cbed97bb34eee5df27ffabaa7ef8a092e9
manuasir <manu.asi2@gmail.com>
2017-08-30 19:20:45 +0200

    fixing all

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-var Nodo            = require('../private/models/model.js');
+
 var path = require('path');
 var express = require('express');
 var router = express.Router();
@@ -12,7 +12,6 @@ var functions = require('./functions')
 mongoose.connect('mongodb://manuasir:mongodb@ds147072.mlab.com:47072/heroku_mctx4f0c');
 //mongoose.connect('mongodb://localhost:27017/crawler');
 
-
 /**
  * Renderiza la vista principal con cabeceras,footer,etc
  */
@@ -24,33 +23,28 @@ router.get('/', function(req, res, next) {
  * Ruta que comienza a procesar una cadena
  */
 router.get('/crawl/', function(req, res){
-  var url=""
-  if(!req.query.q || !req.query.level)
+  if(!req.query.q || !req.query.level || !req.query.srv)
     return res.status(400).json({code:"faltan parametros de busqueda"});
-  var string = req.query.q;
-  var level = (req.query.level < 5) ? req.query.level : 5;
-  console.log("nivel...",level)
-  if(string.indexOf("/") > -1) {
-    url = 'http://'+string;
-  }
-  else
-    url = 'http://'+string+"/";
 
-  var arania = new Crawler(url,level);
-  console.log('arrancando')
-  arania.arrancar(arania.getPrimeraUrl(),arania.getArbol(),-1,level,true,function(err,data){
+  var url = util.urlValid(req.query.q);
+  var level = (req.query.level < 300) ? req.query.level : 300;
+  var srv = req.query.srv;
+  var crawl = new Crawler(url,level);
+
+  // cargar configuración predeterminada (ej. blogspot)
+  var conf = confs.services(srv)
+
+  crawl.arrancar(crawl.getPrimeraUrl(),crawl.getArbol(),-1,level,conf,function(err,data){
     if(err){
       console.error("terminado con error ",err)
       return res.status(401).json(err);
     }
-    console.log("terminado sin error, guardando en mongodb ")
-    arania.insertTreeIntoDb(function(err,data){
+    crawl.insertTreeIntoDb(function(err,data){
       if(err)
         return res.status(402).json(err);
       // el join une los resultados
-      var aux = _.join(_.map(functions.getPayloads(data),'payload'))
+      var aux = _.map(functions.getPayloads(data),'payload')
       res.status(200).json(aux);
-
     });
   })
 });
