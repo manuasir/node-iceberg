6c7eacead81d3f606d810932247042d66d7f5504
manuasir <manu.asi2@gmail.com>
2017-11-14 04:21:52 +0100

    testing filters

---
+++ test/filter/testFilter.js
@@ -0,0 +1,35 @@
+/* global describe it */
+
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const Filter = require('../../lib/classes/filter')
+const assert = require('assert')
+const _ = require('lodash')
+const Iceberg = require('../../lib/classes/iceberg')
+const confs = require('../../plugins/configurations')
+
+describe('testing functions', () => {
+  it('load module', (done) => {
+    assert(Filter)
+    done()
+  })
+  it(' filtering payload', async () => {
+    try {
+
+      const scraper = new Iceberg('https://estacion-katowice.blogspot.com')
+      let conf = confs.services('blogspot')
+
+      let DOM = await scraper.getDocumentData('https://estacion-katowice.blogspot.com')
+      const filter = new Filter(DOM)
+      expect(filter).to.be.a('object')
+      let pay = filter.getElementsByFilter(conf.selector)
+      expect(pay).to.be.a('array')
+      expect(filter.getFilteredHrefsWithAttribs(conf.iteratorElement)).to.be.a('object')
+
+    } catch(err){
+      throw err
+    }
+  })
+})
